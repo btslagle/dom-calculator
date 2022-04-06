42a7ade30f9e2d2152ada6aaab606391f0766917
Brad Slagle <btslagle@gmail.com>
2022-04-06 16:59:35 -0500

    Update calculator

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+let acceptingInput = true
 const screen = document.querySelector("#screen")
 const buttons = document.querySelectorAll("span")
 const clear = document.querySelector("#clear")
@@ -15,6 +16,21 @@ buttons.forEach(button => {
         }
         if (button.textContent !== "÷" && button.textContent !== "=" && button.textContent !== "x" && button.textContent !== "C") {
             screen.textContent += button.textContent
+        }
+        try {
+            if (button.textContent === "=" && acceptingInput) {
+                if (screen.textContent.includes("/0" || "//" || "**" || "++" || "--")) {
+                    screen.textContent = "Error, Resetting"
+                }
+                screen.textContent = eval(screen.textContent)
+            }
+        } catch (error) {
+            screen.textContent = "Error, Resetting"
+            acceptingInput = false
+            setTimeout(function() {
+                acceptingInput = true
+                screen.textContent = ""
+            }, 1000)
         }
     })
 })
